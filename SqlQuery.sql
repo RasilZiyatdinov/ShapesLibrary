93e14e9b1380e614547a65c59b576de64a889737
RasilZiyatdiniv <ziyatdinov.rasil@mail.ru>
2023-10-04 14:42:40 +0500

    Add SQL query

---
+++ SqlQuery.sql
@@ -0,0 +1,21 @@
+﻿/*Предполагается что в базе данных есть две таблицы
+"Products" и "Categories" с атрибутами Id, Name
+так как одному продукту может соответствовать много категорий, 
+а в одной категории может быть много продуктов, то наблюдается связь многие ко многим
+между продуктами и категориями
+следователно, в базе необходимо хранить составную таблицу 
+"ПродуктыКатегории" с составным первичным ключом для обхода данной связи
+*/
+
+CREATE TABLE ProductCategories (
+	ProductId INT FOREIGN KEY REFERENCES Products(Id),
+	CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+	PRIMARY KEY (ProductId, CategoryId)
+);
+
+SELECT P."Name", C."Name"
+FROM Products P
+LEFT JOIN ProductCategories PC
+	ON P.Id = PC.ProductId
+LEFT JOIN Categories C
+	ON PC.CategoryId = C.Id;
